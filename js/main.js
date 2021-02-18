62dc889eab3c4a9039cd4ba5d6ddcf71e7ec0850
José Dagmar Florentino da Silva Sobrinho <64164006+Dagmar87@users.noreply.github.com>
2021-02-18 16:35:21 -0300

    Aula02

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-var nome = "José Dagmar FSS";
+// Aula 02: Array e Dicionário
+
+/*
+var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+var lista = ["maçã", "pera", "laranja"];
+console.log(lista);
+console.log(lista[1]);
+alert(lista[1]);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+
+// Aula 01: Introdução ao JavaScript
+
+/*
+ar nome = "José Dagmar FSS";
 var idade = 33;
 var idade2 = 10;
 var frase = "O Japão é o melhor time do mundo";
@@ -6,13 +34,13 @@ var frase = "O Japão é o melhor time do mundo";
 var n1 = 5;
 var n2 = 3;
 
-//alert(nome + " tem " + idade + " anos");
+alert(nome + " tem " + idade + " anos");
 
-//alert(idade + idade2);
+alert(idade + idade2);
 
-//alert("Bem vindo " + nome);
+alert("Bem vindo " + nome);
 
-//alert("Meu Primeiro JS");
+alert("Meu Primeiro JS");
 
 console.log(nome);
 console.log(idade + idade2);
@@ -22,3 +50,4 @@ console.log(frase.toUpperCase());
 console.log(frase.toLowerCase());
 
 console.log(n1 * n2);
+*/
