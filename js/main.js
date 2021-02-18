e1f6a7465540286c2f7bea53ac318031a3870aab
José Dagmar Florentino da Silva Sobrinho <64164006+Dagmar87@users.noreply.github.com>
2021-02-18 11:47:20 -0300

    aula01

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var nome = "José Dagmar FSS";
+var idade = 33;
+var idade2 = 10;
+var frase = "O Japão é o melhor time do mundo";
+
+var n1 = 5;
+var n2 = 3;
+
+//alert(nome + " tem " + idade + " anos");
+
+//alert(idade + idade2);
+
+//alert("Bem vindo " + nome);
+
+//alert("Meu Primeiro JS");
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão","Brasil"));
+//alert(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+console.log(n1 * n2);
